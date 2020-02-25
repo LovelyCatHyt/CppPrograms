997dcebecefc03d45269894aac77e1bfe3dacf8a
Hyt <69683382@qq.com>
2020-02-25 11:20:36 +0800

    课堂练习
    三个数求最大,由于还没搞懂模板的用法,也不想写排序函数,就手动写了个冒泡.测试了1 2 3输出是对的,那应该没问题了🙂

---
+++ CppPrograms/CourseTest01/CourseTest01.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	int a, b, c;
+	cout << "Please enter three number." << endl;
+	cin >> a >> b >> c;
+	if (a < b)
+	{
+		std::swap(a, b);
+	}
+	if (b < c)
+	{
+		std::swap(b, c);
+	}
+	if (a < b)
+	{
+		std::swap(a, b);
+	}
+	cout << "Answer is:\na:" << a << "\nb:" << b << "\nc:" << c;
+	return 0;
+}
