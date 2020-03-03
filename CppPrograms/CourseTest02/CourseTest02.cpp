6c2b968aea9543bde79f4b979946c5ddcb216609
Hyt <69683382@qq.com>
2020-03-03 08:41:42 +0800

    第二次课堂作业
    求最大, 试用了一下函数模板和命名空间, 感觉还行.

---
+++ CppPrograms/CourseTest02/CourseTest02.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+namespace MyTools
+{
+	template<typename T>
+	T Max(T* toFindArray, int size)
+	{
+		T temp = toFindArray[0];
+		for (int i = 0; i < size; i++)
+		{
+			if (temp < toFindArray[i])
+			{
+				temp = toFindArray[i];
+			}
+		}
+		return temp;
+	}
+}
+int main()
+{
+	int n = 0;
+	std::string* list = NULL;
+	cout << ">Please enter the number of the string list:";
+	cin >> n;
+	list = new string[n];
+	cout << ">Then enter " << n << " line of strings:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> list[i];
+	}
+	string max = MyTools::Max<string>(list, n);
+	cout << ">max:" << max<<endl;
+	delete []list;
+	return 0;
+}
