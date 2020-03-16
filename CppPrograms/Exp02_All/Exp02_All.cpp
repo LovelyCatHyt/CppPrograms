ed32f0baae7287b69e1bc93f84e1954f22ea4141
Hyt <69683382@qq.com>
2020-03-16 23:32:55 +0800

    修正输出格式
    回头看我的cmd发现原来我格式搞错了, 应该是用户输入项前面有个'>'

--- CppPrograms/Exp02_All/Exp02_All.cpp
+++ CppPrograms/Exp02_All/Exp02_All.cpp
@@ -13,7 +13,7 @@ int gcd(int m, int n)
 int main()
 {
 	int a=1, b=1;
-	cout << ">Please enter two number a and b: " << endl;
+	cout << "Please enter two number a and b: " << endl<<">";
 	cin >> a >> b;
 	if (a < b) std::swap(a, b);
 	cout << ">gcd(" << a << "," << b << ") = " << gcd(a, b);
@@ -30,26 +30,25 @@ using std::endl;
 int main()
 {
 	int n = 0;
-	cout << ">Please enter n: " << endl;
+	cout << "Please enter n: " << endl << ">";
 	cin >> n;
 	int tryCount = 1;
 	while (n < 50 || n>10000)
 	{
 		if (tryCount > 4)
 		{
-			cout << ">You've tried too many times!";
+			cout << "You've tried too many times!";
 			return 0;
 		}
-		cout << ">The number you enter is invalid! Enter again ("<<tryCount<<"/5): " << endl;
+		cout << "The number you enter is invalid! Enter again ("<<tryCount<<"/5): " << endl<<">";
 		cin >> n;
 		tryCount++;
 	}
 	int sum = 0;
 	for (int i = 1; i <= n; sum += i++);
-	cout << ">The sum is: " << sum<<endl;
+	cout << "The sum is: " << sum<<endl;
 	return 0;
 }
-
 
 //2.
 #include <iostream>
@@ -57,20 +56,26 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-float GetCost(int consume)
+float GetCost(float consumePerOne)
 {
 	//一行处理一个区间
-	if (consume < 20) return consume * 0.6;
-	if (consume < 30) return consume - 8;//12+(x-20)=x-8
-	return 2 * consume - 38;//22+2(x-30)=2x-38
+	if (consumePerOne < 20) return consumePerOne * 0.6;
+	if (consumePerOne < 30) return consumePerOne - 8;//12+(x-20)=x-8
+	return 2 * consumePerOne - 38;//22+2(x-30)=2x-38
 }
 
 int main()
 {
-	int consume = 0;
-	cout << ">Please enter consume: " << endl;
+	float consume = 0;
+	int count = 1;
+	float temp = 0;
+	cout << "Please enter total consume: " << endl<<">";
 	cin >> consume;
-	cout << ">Cost is: " << GetCost(consume)<<endl;
+	cout << "Please enter the number of people:" << endl<<">";
+	cin >> count;
+	temp = GetCost(consume / count);
+	cout << "Cost per people is: " << temp << endl;
+	cout << "Total cost: " << temp * count << endl;
 }
 
 //1.
@@ -83,16 +88,17 @@ using std::endl;
 int main()
 {
 	double a, b, c;
-	cout << ">Please enter three sides of the triangle:" << endl;
+	cout << "Please enter three sides of the triangle:" << endl<<">";
 	cin >> a >> b >> c;
 	//输入数据检查, 错误则重新输入
 	while ((a + b <= c) || (a - b >= c) || (b - a >= c))
 	{
-		cout << ">Not valid triangle! Please re-enter the values of the three edges:" << endl;
+		cout << "Not valid triangle! Please re-enter the values of the three edges:" << endl<<">";
 		cin >> a >> b >> c;
 	}
 	//代入公式
 	double s = (a + b + c)/2;
-	cout << ">The area of the triangle is: " << sqrt(s * (s - a) * (s - b) * (s - c)) << endl;
+	cout << "The area of the triangle is: " << sqrt(s * (s - a) * (s - b) * (s - c)) << endl;
 }
+
 */
