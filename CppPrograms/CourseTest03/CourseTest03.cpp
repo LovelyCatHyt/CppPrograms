e49b97fb5e551031ee1318d74a09b688e658d96d
Hyt <69683382@qq.com>
2020-03-18 18:57:28 +0800

    第三个录入解决方案的课堂作业: swap的魔法 std里也有swap 所以不能随便using

---
+++ CppPrograms/CourseTest03/CourseTest03.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	int* p1, * p2, * p3;
+	void exchange(int* p1, int* p2, int* p3);
+	void swap(int* p1, int* p2);//Doesn't work as expected.
+	cout << "Please Input Three number:" << endl;
+	cin >> a >> b >> c;
+	p1 = &a;
+	p2 = &b;
+	p3 = &c;
+	exchange(p1, p2, p3);//What's the problem after the calling?
+	cout << "The Sorted Numbers are: " << endl;
+	cout << "a=" << *p1 << endl << "b=" << *p2 << endl << "c=" << *p3 << endl;
+	return 0;
+}
+void exchange(int* p1, int* p2, int* p3)
+{
+	if (*p1 < *p2) swap(p1, p2);//Bug occurs: "swap" is "std::swap" instead of "::swap"
+	if (*p1 < *p3) swap(p1, p3);
+	if (*p2 < *p3) swap(p2, p3);
+	cout << "The Sorted Numbers are: " << endl;
+	cout << "a=" << *p1 << endl << "b=" << *p2 << endl << "c=" << *p3 << endl;
+}
+void swap(int* p1, int* p2)
+{
+	int temp;
+	temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
--- CppPrograms/TestSomething/TestSomething.cpp
+++ CppPrograms/TestSomething/TestSomething.cpp
@@ -1,55 +1,12 @@
-﻿//#include <iostream>
-//using namespace std;
-//
-//class Point
-//{
-//	int index;
-//	int x, y;
-//	void Init()
-//	{
-//		cout << "Point[" << index << "] initialized." << endl;
-//	}
-//public:
-//	static int count;
-//	Point()
-//	{
-//		x = y = 0;
-//		index = count++;
-//		Init();
-//	}
-//	Point(int x, int y)
-//	{
-//		this->x = x;
-//		this->y = y;
-//		index = count++;
-//		Init();
-//	}
-//};
-//
-//int Point::count;
-//
-//int main()
-//{
-//	Point orgin;
-//	return 0;
-//}
-
-#include<iostream>
-#include<math.h>
+﻿#include <iostream>
 using namespace std;
-
-void f(int a[])
-{
-	int b;
-	cout << "int[]: " << sizeof(a) << endl;
-	cout << "int:   " << sizeof(int) << endl;
-	cout << "int *: " << sizeof(&b) << endl;
-	cout << "long long: " << sizeof((long long)1) << endl;
-}
 
 int main()
 {
-	int a[3];
-	f(a);	
-	return 0;
+	int i = 0;
+	while (cin>>i)
+	{
+		cout << i << endl;
+	}
+	cout << "finished.";
 }
