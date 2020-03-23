b8a9834b3e32e1732dab2de565e97228c1a2d57e
Hyt <69683382@qq.com>
2020-03-23 20:48:58 +0800

    第四次课堂实验:运算符重载
    C++的左值右值挺有意思, 就是太难理解了
    (上次Commit错了再commit一次)

---
+++ CppPrograms/CourseTest04/CourseTest04.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+using std::ostream;
+//复数
+class Com
+{
+private:
+	double real, image;
+public:
+	Com()
+	{
+		real = image = 0;
+	}
+	Com(double real, double image = 0) :real(real), image(image)
+	{
+	}
+	Com operator +(const Com r) const
+	{
+		return Com(real + r.real, image + r.image);
+	}
+	Com operator -(const Com r) const
+	{
+		return Com(real - r.real, image - r.image);
+	}
+	Com operator *(const Com r) const
+	{
+		return Com(real * r.real - image * r.image, real * r.image + image * r.real);
+	}
+	Com operator =(double real)
+	{
+		return Com(real);
+	}
+	friend ostream& operator<<(ostream& out, const Com& toOut);
+};
+ostream& operator<<(ostream& out, const Com& toOut)
+{
+	(toOut.real == 0 ? cout : (cout << toOut.real)) << (toOut.image >= 0 ? "+" : "") << toOut.image << 'i';
+	return out;
+}
+int main()
+{
+	Com a;
+	a = 1;
+	Com b(1, -3);
+	cout << "a    =" << a << endl;
+	cout << "b    =" << b << endl;
+	cout << "a+b  =" << (a + b) << endl;
+	cout << "a-b  =" << (a - b) << endl;
+	cout << "a*b  =" << (a * b) << endl;
+	return 0;
+}
